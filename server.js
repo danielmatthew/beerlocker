69c4c1e17e7ae5818b1dbad842cb8f7773375238
Daniel Matthew <hello@danmatthew.co.uk>
2015-01-27 11:27:08 +0000

    CRUD functionality
    Using body-parser module to help with post/put requests

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 // Get packages
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var Beer = require('./models/beer');
 
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost:27017/beerlocker');
 
 // Create new express app
 var app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: true;
+}));
+
 // Use environment-defined port or 3000
 var port = process.env.PORT || 3000;
 // Create Express router
@@ -15,6 +22,65 @@ var router = express.Router();
 router.get('/', function(req, res){
   res.json({
     message: "You are running low on beer!"
+  });
+});
+
+var beersRoute = router.route('/beers');
+var beerRoute = router.route('/beers/:beer_id');
+
+beersRoute.post(function(req, res) {
+  var beer = new Beer();
+
+  // Set properties from POST data
+  beer.name = req.body.name;
+  beer.type = req.body.type;
+  beer.quantity = req.body.quantity;
+
+  // Save beer and check for errors
+  beer.save(function(err) {
+    if (err) {
+      res.send(err);
+    }
+
+    res.json({message: 'Beer added to the locker', data: beer});
+  });
+});
+
+beersRoute.get(function(req, res) {
+  Beer.find(function(err, beers) {
+    if (err) {
+      res.end(err);
+    }
+
+    res.json(beers);
+  });
+});
+
+beerRoute.get(function(req, res) {
+  Beer.findById(req.params.beer_id, function(err, beer) {
+    if (err) {
+      res.send(err);
+    }
+
+    res.json(beer);
+  });
+});
+
+beerRoute.put(function(req, res) {
+  Beer.findById(req.params.beer_id, function(err, beer) {
+    if (err) {
+      res.send(err);
+    }
+
+    beer.quantity = req.body.quantity;
+
+    beer.save(function(err) {
+      if (err) {
+        res.send(err);
+      }
+
+      res.json(beer);
+    });
   });
 });
 
